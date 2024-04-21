075089795238a70145a1878d42c05a88ce59f5bd
Ben Fry <ben@fathom.info>
2024-04-20 21:22:29 -0400

    working on lister script

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.11
 
 '''
 Cleanup script to deal with Apple Mail placing thousands of emails in
@@ -34,6 +34,33 @@ import sys
 import imapdedup
 
 
+def handle_account(key, source_options, work_path):
+    list_path = os.path.join(work_path, 'mailboxes.json')
+    # source_id_path = os.path.join(work_path, 'source-ids.txt')
+    # target_id_path = os.path.join(work_path, 'target-ids.txt')
+    list_path = os.path.join(work_path, 'messages.tsv')
+
+    if not os.path.exists(list_path):
+        print(f'Saving list of mailboxes to {list_path}')
+        print('Remove any entries from that file, and run again to create a list of IDs')
+        options = [
+            "--save-list", list_path
+        ]
+        imapdedup.process(*imapdedup.get_arguments(source_options + options))
+
+    elif not os.path.exists(list_path):
+        # print(f'Writing a list of all message IDs from the source to {source_id_path}')
+        # print('Run again to create the list of IDs on the target.')
+        options = [
+            "--save-list", list_path,
+            "--dry-run"  # don't mark any found items for deletion
+        ]
+        with open(list_path, 'r') as f:
+            mboxes = json.load(f)
+
+        imapdedup.process(*imapdedup.get_arguments(source_options + options + mboxes))
+
+
 def handle(config_path):
     if not os.path.exists(config_path):
         print(f'{config_path} does not exist')
@@ -48,73 +75,7 @@ def handle(config_path):
 
     for key, args in parsed.items():
         print(key)
-        # source_options = parsed['source']
-        # target_options = parsed['target']
-
-    '''
-    list_path = os.path.join(work_path, 'mailboxes.json')
-    source_id_path = os.path.join(work_path, 'source-ids.txt')
-    target_id_path = os.path.join(work_path, 'target-ids.txt')
-
-    if not os.path.exists(list_path):
-        print(f'Saving list of mailboxes to {list_path}')
-        print('Remove any entries from that file, and run again to create a list of IDs')
-        options = [
-            "--save-list", list_path
-        ]
-        imapdedup.process(*imapdedup.get_arguments(source_options + options))
-
-    elif not os.path.exists(source_id_path):
-        print(f'Writing a list of all message IDs from the source to {source_id_path}')
-        print('Run again to create the list of IDs on the target.')
-        options = [
-            "--save-ids", source_id_path,
-            "--dry-run"  # don't mark any found items for deletion
-        ]
-        with open(list_path, 'r') as f:
-            mboxes = json.load(f)
-
-        imapdedup.process(*imapdedup.get_arguments(source_options + options + mboxes))
-
-    elif not os.path.exists(target_id_path):
-        print(f'Writing a list of all recovered message IDs on the target at {source_id_path}')
-        print('Run again to compare the two lists.')
-        options = [
-            "--save-ids", target_id_path,
-            "--dry-run"  # don't mark any found items for deletion
-        ]
-        mboxes = [ 'recovered' ]
-
-        imapdedup.process(*imapdedup.get_arguments(target_options + options + mboxes))
-
-    else:
-        options = [
-            "--delete-ids", source_id_path,
-            # "--dry-run"  # don't mark any found items for deletion
-        ]
-        mboxes = [ 'recovered' ]
-
-        imapdedup.process(*imapdedup.get_arguments(target_options + options + mboxes))
-
-        #
-
-        print('List of message IDs not found in both locations:')
-        gmail = set()
-        with open(source_id_path) as f:
-            for line in f:
-                gmail.add(line.strip())
-
-        recovered = set()
-        with open(target_id_path) as f:
-            for line in f:
-                recovered.add(line.strip())
-        recovered_count = len(recovered)
-
-        remaining = recovered - gmail
-        # for item in remaining:
-        #     print(item)
-        print(f'Found {len(remaining):,} unmwatched out of {recovered_count:,}')
-    '''
+        handle_account(key, args, work_path)
 
 
 if __name__ == "__main__":
